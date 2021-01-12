116ec125a06c7e5d9272f380476a2ef25d7d8c9d
Charlie Bertrand <charlie.bertrand@live.com>
2021-01-12 21:18:24 +0100

    correcting backoffice with relations in offer & user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # validates :first_name, :last_name, presence: true
   # has_many :bookings
-  # has_many :offers
+  has_many :offers
 end
