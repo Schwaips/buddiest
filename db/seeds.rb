c4ff4b80b607e4e476e5b6fed6edb65fe87cee1a
Charlie Bertrand <charlie.bertrand@live.com>
2021-01-12 20:50:43 +0100

    seeding offers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-puts "---creating users"
+# puts "---creating users"
 
-  raph = User.new(email: "raphael.mosca@gmail.com", password: "123456", first_name: "Raphael", last_name: "Mosca", address:"rue batman 40000 angers", nationality: "French")
-  raph.save!
-  puts "Creating raph"
+#   raph = User.new(email: "raphael.mosca@gmail.com", password: "123456", first_name: "Raphael", last_name: "Mosca", address:"rue batman 40000 angers", nationality: "French")
+#   raph.save!
+#   puts "Creating raph"
 
-  olivia = User.new(email: "olivia.bihl@gmail.com", password: "123456", first_name: "Olivia", last_name: "Bihl", address:"Montreuil paris", nationality: "French")
-  olivia.save!
-  puts "Olivia created"
+#   olivia = User.new(email: "olivia.bihl@gmail.com", password: "123456", first_name: "Olivia", last_name: "Bihl", address:"Montreuil paris", nationality: "French")
+#   olivia.save!
+#   puts "Olivia created"
 
-  amelien = User.new(email: "amelien.delahaie@gmail.com", password: "123456", first_name: "Amelien", last_name: "Delahaie", address:"Levallois", nationality: "French")
-  amelien.save!
-  puts "Amélien created"
+#   amelien = User.new(email: "amelien.delahaie@gmail.com", password: "123456", first_name: "Amelien", last_name: "Delahaie", address:"Levallois", nationality: "French")
+#   amelien.save!
+#   puts "Amélien created"
 
-puts "end of user creation"
+# puts "end of user creation"
 
 
+puts "---starting seeding offers---"
+10.times do
+ user = User.all.sample
+ user_id = User.all.sample.id
+ price = [25, 46, 100, 10].sample
+ area = [5, 10, 15, 20, 100].sample
+  offer = Offer.new(title: Faker::Educator.course_name, description: Faker::Educator.subject, price_per_hour: price, area: area, user_id: user_id)
+  offer.save!
+  puts "saving #{offer.title}"
+end
+puts "---end of seeding offers"
 
