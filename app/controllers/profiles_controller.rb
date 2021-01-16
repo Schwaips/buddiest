e451f6e63696bb0dde25873388bdd5edd90e9ae1
Charlie Bertrand <charlie.bertrand@live.com>
2021-01-16 11:35:39 +0100

    correcting update for profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,20 @@
+class ProfilesController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    @user = current_user
+    skip_authorization
+    @user.update(params_user)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def params_user
+     params.require(:user).permit(:username, :first_name, :last_name, :address, :phone_number, :date_of_birth, :nationality, :gender, :avatar)
+  end
+
+end
+
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  
+
   # GET /resource/sign_up
   # def new
   #   super
@@ -24,7 +24,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
     if @user.update(params_user)
      flash[:alert] = "Votre compte est à jour"
-    #  redirect_to dashboard_path
+     return redirect_to dashboard_path
     else
       flash[:alert] = "Echec dans la mise à jour de votre compte"
     end
