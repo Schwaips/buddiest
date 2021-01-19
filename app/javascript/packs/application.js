3d983291c585711e569b828e0f577ebd5a6a7157
amelien delahaie <amelien.delahaie@gmail.com>
2021-01-19 21:00:44 +0100

    work on the show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,10 @@ require("channels")
 // External imports
 import "bootstrap";
 
+// External flat pickr
+import "../plugins/flatpickr"
+
+
 // Internal imports, e.g:
 import { initMapbox } from '../plugins/init_mapbox';
 
@@ -39,4 +43,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initUpdateNavbarOnScroll();
+
 });
