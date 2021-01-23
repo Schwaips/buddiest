d993f9e6add7959a4c483ff35391aaa6ae683d1f
Charlie Bertrand <charlie.bertrand@live.com>
2021-01-23 13:18:25 +0100

    dash

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
   validates :status, inclusion: { in: ["Validé", "En attente", "Refusé"] }
   validates :date_begin, :date_end, presence: true
   
+
   def total_price
     (date_end - date_begin) / 1.hour * offer.price_per_hour
   end
