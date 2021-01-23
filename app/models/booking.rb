a825430738e9881a050a9e4a502da6171cd581e0
Olivia Bihl <olivia.bihl@gmail.com>
2021-01-23 11:20:10 +0100

    working on price counter

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,8 +3,12 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :status, inclusion: { in: ["Validé", "En attente", "Refusé"] }
   validates :date_begin, :date_end, presence: true
-  
+
   def total_price
-    (date_end - date_begin) / 1.hour * offer.price_per_hour
+    if date_begin.nil? || date_end.nil?
+      "Sélectionnez des dates pour connaitre le prix"
+    else
+      (date_end - date_begin) / 1.hour * offer.price_per_hour
+    end
   end
 end
